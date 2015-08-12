a8de250c95f318d439d6a171aae17eb5a18e1c92
rudiments <fb53ice4j@gmail.com>
2015-08-12 19:20:07 +0000

    this is a 2ch_clone app

---
+++ app/models/board.rb
@@ -0,0 +1,6 @@
+class Board < ActiveRecord::Base
+ has_many :comments, dependent: :destroy
+ 
+ validates :title, presence: true, length: { in: 4..127 }
+ validates :editor, presence: true, length: { in: 1..30 }
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+ belongs_to :board
+
+ validates :board, presence: true
+ validates :name, presence: true
+ validates :content, presence: true, length: { in: 1..1000 }
+end
